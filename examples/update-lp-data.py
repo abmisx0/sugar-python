d43237b204dc2899e4c823179154d7f5a693d29b
abmis <88830544+abmisx0@users.noreply.github.com>
2024-11-01 12:10:42 -0700

    added update lp data example

---
+++ examples/update-lp-data.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from typing import Literal
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from sugar import Sugar
+
+
+def update_lp_data(chain: Literal["base", "op"]):
+    """Update lp data for specified chain."""
+    sugar = Sugar(chain)
+    sugar.lp_tokens(listed=False)
+    sugar.lp_all()
+
+
+if __name__ == "__main__":
+    for chain in ("base", "op"):
+        update_lp_data(chain)
